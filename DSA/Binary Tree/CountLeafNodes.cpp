59d1a8d8d4bb55b8fdd3733c22a98cf802df5a9e
Saif Abrar <saifabrar.1998@gmail.com>
2024-01-04 23:09:16 +0600

    binary tree code ninjas problem solve

---
+++ DSA/Binary Tree/CountLeafNodes.cpp
@@ -0,0 +1,127 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int val;
+    Node *left;
+    Node *right;
+    Node(int val)
+    {
+        this->val = val;
+        this->left = NULL;
+        this->right = NULL;
+    }
+};
+
+Node *inputTree()
+{
+    int val;
+    cin >> val;
+    Node *root;
+    if (val == -1)
+        root = NULL;
+    else
+    {
+        root = new Node(val);
+    }
+
+    queue<Node *> q;
+
+    if (root)
+        q.push(root);
+
+    while (!q.empty())
+    {
+        Node *p = q.front();
+        q.pop();
+
+        int l, r;
+        cin >> l >> r;
+
+        Node *left;
+        Node *right;
+
+        if (l == -1)
+        {
+            left = NULL;
+        }
+        else
+        {
+            left = new Node(l);
+        }
+
+        if (r == -1)
+        {
+            right = NULL;
+        }
+        else
+        {
+            right = new Node(r);
+        }
+
+        p->left = left;
+        p->right = right;
+
+        if (p->left)
+            q.push(p->left);
+        if (p->right)
+            q.push(p->right);
+    }
+    return root;
+}
+
+void levelOrder(Node *root)
+{
+    if (root == NULL)
+    {
+        cout << "NO TREE AVAILABLE" << endl;
+        return;
+    }
+
+    queue<Node *> q;
+    q.push(root);
+
+    while (!q.empty())
+    {
+        // step 1. ber kore anlam
+        Node *p = q.front();
+        q.pop();
+
+        // step 2. kaj
+        cout << p->val << " ";
+
+        // step 3
+        if (p->left)
+            q.push(p->left);
+        if (p->right)
+            q.push(p->right);
+    }
+}
+
+int CountLeafNodes(Node *root)
+{
+    if (root == NULL)
+    {
+        return 0;
+    }
+    if (root->left == NULL && root->right == NULL)
+    {
+        return 1;
+    }
+    else
+    {
+        int l = CountLeafNodes(root->left);
+        int r = CountLeafNodes(root->right);
+        return r + l;
+    }
+}
+
+int main()
+{
+    Node *root = inputTree();
+    // levelOrder(root);
+    cout << CountLeafNodes(root) << endl;
+    return 0;
+}
